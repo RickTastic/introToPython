475edda6734dc4f886f17b82cc77aa6173869081
Ash <raulramsunder@gmail.com>
2018-12-23 17:11:03 +0200

    Guessing Game: Added random number seed

--- venv/projectScripts/guessingGame.py
+++ venv/projectScripts/guessingGame.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-computerRandomNumber = 73
+computerRandomNumber = random.randrange(0, 100)
 print("I have selected a random number between 0 and 100. Your job is to try and guess the number.")
 print("I will provide you with tips of the number I have selected. To quit press q")
 print("Please enter your first try: ")
