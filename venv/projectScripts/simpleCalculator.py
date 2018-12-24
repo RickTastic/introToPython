baa719c1e74b3a7f98e1712b7f1e3ade9150fef7
Ashai <raulramsunder@gmail.com>
2018-12-24 21:38:01 +0200

    Simple Calculator: Added the following methods - getInput() - report() - add() and subtract()

--- venv/projectScripts/simpleCalculator.py
+++ venv/projectScripts/simpleCalculator.py
@@ -1,3 +1,8 @@
+# Global variable
+result = 0.0
+value1 = 0.0
+value2 = 0.0
+
 def help():
   print("Add: Adds two numbers")
   print("Subtract: Subtracts two numbers")
@@ -8,24 +13,39 @@ def help():
 def menu():
   return input("=== A)dd S)ubtract P)rint H)elp Q)uit ===")
 
+def getInput():
+  global value1, value2
+  value1 = float(input("Enter value 1: "))
+  value2 = float(input("Enter value 2: "))
+
+def report():
+  print(result)
+
+def add():
+  global result
+  result = value1 + value2
+
+def subtract():
+  global result
+  result = value1 - value2
+
 def main():
-  result = 0.0
   done = False;
   while not done:
     choice = menu()
     if choice == 'A' or choice == 'a':
-      value1 = float(input("Enter value 1: "))
-      value2 = float(input("Enter value 2: "))
-      result = value1 + value2
-      print(result)
+      getInput()
+      add()
+      report()
     elif choice == 'S' or choice == 's':
-      value1 = float(input("Enter value 1: "))
-      value2 = float(input("Enter value 2: "))
-      result = value1 - value2
-      print(result)
+     getInput()
+     subtract()
+     report()
     elif choice == 'H' or choice == 'h':
       help()
     elif choice == 'P' or choice == 'p':
-      print(result)
+      report()
     else:
       done = True
+
+main()
