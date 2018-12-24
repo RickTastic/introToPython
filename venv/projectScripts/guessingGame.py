fd126e1afe6f0d01d0ca43f4d876f07eb40305eb
Ash <raulramsunder@gmail.com>
2018-12-24 15:23:31 +0200

    Guessing Game: Added documentation

--- venv/projectScripts/guessingGame.py
+++ venv/projectScripts/guessingGame.py
@@ -1,6 +1,5 @@
+# This script is a basic guessing game
 import random
-import math
-
 computerRandomNumber = random.randrange(0, 100)
 print("I have selected a random number between 0 and 100. Your job is to try and guess the number.")
 print("I will provide you with tips of the number I have selected. To quit press q")
