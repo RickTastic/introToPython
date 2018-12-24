93fcd5304e89d4d1e8d7b6932cdf765554c4d448
Ashai <raulramsunder@gmail.com>
2018-12-24 22:16:53 +0200

    - Removed timeit.py
    - countdown.py: Initial commit
    - recursion.py: Initial commit

--- venv/projectScripts/timeit.py
+++
@@ -1,7 +0,0 @@
-import time
-
-print("Enter your name: ", end = "")
-startTime = time.perf_counter()
-name = input()
-elapsed = time.perf_counter() - startTime
-print(name, "it took you", elapsed, "seconds to respond")
