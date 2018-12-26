d5a74524789ffde48c0f725e6f0ff3a557a45305
Ashai <raulramsunder@gmail.com>
2018-12-26 19:45:57 +0200

    Countdown.py: Changed indentations to two spaces

--- venv/projectScripts/countdown.py
+++ venv/projectScripts/countdown.py
@@ -1,8 +1,8 @@
 # This function demonstrates the concept of a default argument
 
 def countdown(n = 10):
-    for i in range(n, -1, -1):
-        print(i)
+  for i in range(n, -1, -1):
+    print(i)
 
 # Use default argument
 countdown()
