5f6075b9a3c5c11126d04cd9d991f585a94d92d3
Ashai <raulramsunder@gmail.com>
2018-12-27 22:34:22 +0200

    checkTypes.py - Elementary parser and handler for the console input - added error handing, messages and validity - parses the input object

--- venv/projectScripts/checkTypes.py
+++ venv/projectScripts/checkTypes.py
@@ -1,34 +1,28 @@
 import math
+import isInteger
 def checkTypes(guess):
   try:
     message = dict()
-    if type(guess) == int:
-      if guess < 0 or guess > 100:
+    if isInteger.isInteger(guess):
+      if int(guess) < 0 or int(guess) > 100:
         message['errorMessage'] = "Please enter an integer in the range 0...100."
         message['isValid'] = False
+        message['value'] = None
         return message
       else:
         message['errorMessage'] = ""
         message['isValid'] = True
+        message['value'] = int(guess)
         return message
-    elif type(guess) == str and (guess == 'q' or guess == "Q"):
+    elif type(guess) == str and (guess == 'q' or guess == 'Q'):
       message['errorMessage'] = ""
       message['isValid'] = True
-      return message
-    elif type(guess) == float:
-      message['errorMessage'] = "The number " \
-                                + str(guess) \
-                                + " " \
-                                + "is not valid. Did you mean " \
-                                + str(math.floor(guess)) \
-                                + " or " \
-                                + str(math.ceil(guess)) \
-                                + "?"
-      message['isValid'] = False
+      message['value'] = guess
       return message
     else:
       message['errorMessage'] = str(guess) + " is not a valid integer."
       message['isValid'] = False
+      message['value'] = None
       return message
   except Exception:
     raise Exception("Unknown Exception. Please restart the application")
